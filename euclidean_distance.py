0f33c1cbe259eb33da607604792042c6315b0783
Ryan <147990948+qryan13@users.noreply.github.com>
2023-10-15 10:07:44 -0700

    Update euclidean_distance.py
    spacing

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -6,5 +6,6 @@ point_p = (2, 3)
 point_q = (10, 5)
 
 # d(p,q) = sqrt((q1-p1)^2+(q2-p2)^2)
-print('Distance between points (', point_p[0], ',', point_p[1], ') and (', point_q[0], ',', point_q[1], ') is', 
+print('Distance between points (', point_p[0], ',', point_p[1],
+      ') and (', point_q[0], ',', point_q[1], ') is', 
      sqrt(((point_q[0]-point_p[0])**2)+(point_q[1]-point_p[1])**2))
