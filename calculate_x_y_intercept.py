736772c012fedd2efc3add10dfe827930e40dccd
Ryan <147990948+qryan13@users.noreply.github.com>
2023-10-16 05:26:32 -0700

    Add files via upload

---
+++ calculate_x_y_intercept.py
@@ -0,0 +1,9 @@
+# Calculate the slope, x-intercept and y-intercept of y = 2x -5
+# y = mx + b
+# find slope, x intercept and y intercept from m and b
+m = 2
+b = -5
+
+print("The slope is ", m)
+print("The y intercept is ", b)
+print("The x intercept is ", (-b/m))
